3ad45f326b3c882dd931b0c19d0d8f990999a80b
divyansh <divyanshverma689@gmail.com>
2024-07-15 03:27:35 -0400

    updated

--- js.js
+++ js.js
@@ -18,8 +18,8 @@ document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click", ()
 }))
 
 
-var typed = new Typed(".auto-type", {
-    strings: ["Divyansh verma", "Frontend developer", "Programmer"],
+let typed = new Typed(".auto-type", {
+    strings: ["Divyansh verma", "Frontend developer", "Full Stack Developer", "Programmer"],
     typeSpeed: 100,
     backspeed: 200,
     loop: true
